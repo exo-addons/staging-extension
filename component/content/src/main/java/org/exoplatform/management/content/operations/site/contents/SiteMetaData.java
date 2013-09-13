a3d46c3e750c03655024fc372978280865ca6b93
Thomas Delhoménie <thomas.delhomenie@gmail.com>
2013-09-13 12:12:47 +0200

    Move resources handlers in a new submodule

---
+++ component/content/src/main/java/org/exoplatform/management/content/operations/site/contents/SiteMetaData.java
@@ -0,0 +1,40 @@
+package org.exoplatform.management.content.operations.site.contents;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SiteMetaData {
+
+  public static final String SITE_PATH = "site-path";
+  public static final String SITE_WORKSPACE = "site-workspace";
+  public static final String SITE_NAME = "site-name";
+
+  Map<String, String> options = new HashMap<String, String>();
+  Map<String, String> exportedFiles = new HashMap<String, String>();
+  Map<String, String> exportedHistoryFiles = new HashMap<String, String>();
+
+  public Map<String, String> getExportedFiles() {
+    return this.exportedFiles;
+  }
+
+  public Map<String, String> getOptions() {
+    return this.options;
+  }
+
+  public void setExportedFiles(Map<String, String> jcrExportedFiles) {
+    this.exportedFiles = jcrExportedFiles;
+  }
+
+  public void setOptions(Map<String, String> options) {
+    this.options = options;
+  }
+
+  public Map<String, String> getExportedHistoryFiles() {
+    return exportedHistoryFiles;
+  }
+
+  public void setExportedHistoryFiles(Map<String, String> exportedHistoryFiles) {
+    this.exportedHistoryFiles = exportedHistoryFiles;
+  }
+
+}
---
+++ component/ecmadmin/src/main/java/org/exoplatform/management/ecmadmin/operations/templates/nodetypes/NodeTypeTemplatesMetaData.java
@@ -0,0 +1,63 @@
+package org.exoplatform.management.ecmadmin.operations.templates.nodetypes;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.exoplatform.management.ecmadmin.operations.templates.NodeTemplate;
+
+public class NodeTypeTemplatesMetaData {
+
+  private String label;
+  private String nodeTypeName;
+  private boolean documentTemplate;
+  private Map<String, List<NodeTemplate>> templates;
+
+  public NodeTypeTemplatesMetaData() {
+    this.templates = new HashMap<String, List<NodeTemplate>>();
+  }
+
+  public String getLabel() {
+    return label;
+  }
+
+  public void setLabel(String label) {
+    this.label = label;
+  }
+
+  public boolean isDocumentTemplate() {
+    return documentTemplate;
+  }
+
+  public void setDocumentTemplate(boolean documentTemplate) {
+    this.documentTemplate = documentTemplate;
+  }
+
+  public Map<String, List<NodeTemplate>> getTemplates() {
+    return templates;
+  }
+
+  public void setTemplates(Map<String, List<NodeTemplate>> templates) {
+    this.templates = templates;
+  }
+
+  public void addTemplate(String type, NodeTemplate template) {
+    List<NodeTemplate> typeTemplates = templates.get(type);
+    if (typeTemplates == null) {
+      typeTemplates = new ArrayList<NodeTemplate>();
+    }
+
+    typeTemplates.add(template);
+    templates.put(type, typeTemplates);
+  }
+
+  public String getNodeTypeName() {
+    return nodeTypeName;
+  }
+
+  public void setNodeTypeName(String nodeTypeName) {
+    this.nodeTypeName = nodeTypeName;
+  }
+
+}
