070e31b218a5c49fa30060d594673dca0caefcc7
Thomas Delhoménie <thomas.delhomenie@gmail.com>
2013-10-11 21:37:56 +0200

    Use LESS juzu plugin

--- portlet/src/main/java/org/exoplatform/management/portlet/package-info.java
+++ portlet/src/main/java/org/exoplatform/management/portlet/package-info.java
@@ -11,16 +11,26 @@
 )
 @Assets(
   scripts = {
-    @juzu.plugin.asset.Script(id = "angularjs", src = "js/angular.min.js"),
-    @juzu.plugin.asset.Script(id = "staging", src = "js/staging.js")
-})
+    @Script(id = "angularjs", src = "js/angular.min.js"),
+    @Script(id = "staging", src = "js/staging.js")
+  },
+  stylesheets = {
+    @Stylesheet(src = "style/staging.css", location = AssetLocation.APPLICATION)
+  }
+
+)
+@Less("style/staging.less")
 
 package org.exoplatform.management.portlet;
 
 import juzu.Application;
+import juzu.asset.AssetLocation;
 import juzu.plugin.asset.Assets;
+import juzu.plugin.asset.Script;
+import juzu.plugin.asset.Stylesheet;
 import juzu.plugin.binding.Binding;
 import juzu.plugin.binding.Bindings;
+import juzu.plugin.less.Less;
 import juzu.plugin.portlet.Portlet;
 import org.exoplatform.management.service.api.ChromatticService;
 import org.exoplatform.management.service.api.StagingService;
