ee6fdfe99952bc36e8f3b7005b7b7a7b798df516
Thomas Delhoménie <thomas.delhomenie@gmail.com>
2013-11-21 16:52:57 +0100

    Issue #19 : Add automatic selection of the import resources category

--- portlet/src/main/java/org/exoplatform/management/portlet/package-info.java
+++ portlet/src/main/java/org/exoplatform/management/portlet/package-info.java
@@ -3,6 +3,7 @@
 @Bindings(
   {
     @Binding(value = ManagementController.class),
+    @Binding(value = ManagementService.class),
     @Binding(value = StagingService.class, implementation = StagingServiceImpl.class),
     @Binding(value = SynchronizationService.class, implementation = SynchronizationServiceImpl.class),
     @Binding(value = RepositoryService.class),
@@ -42,4 +43,5 @@ import org.exoplatform.management.service.impl.ChromatticServiceImpl;
 import org.exoplatform.management.service.impl.StagingServiceImpl;
 import org.exoplatform.management.service.impl.SynchronizationServiceImpl;
 import org.exoplatform.services.jcr.RepositoryService;
+import org.gatein.management.api.ManagementService;
 import org.gatein.management.api.controller.ManagementController;
