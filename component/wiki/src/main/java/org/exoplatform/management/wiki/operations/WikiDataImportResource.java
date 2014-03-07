875c57600ad02a3788b2de3a32ace882ea06fd37
anhvurz90 <anhvurz90@gmail.com>
2014-03-07 12:02:05 +0700

    WIKI-845 create space in target server in case of nonexisting. Add wiki-extension into packaging

--- component/wiki/src/main/java/org/exoplatform/management/wiki/operations/WikiDataImportResource.java
+++ component/wiki/src/main/java/org/exoplatform/management/wiki/operations/WikiDataImportResource.java
@@ -16,6 +16,8 @@
  */
 package org.exoplatform.management.wiki.operations;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 import java.util.List;
 import java.util.zip.ZipEntry;
@@ -24,8 +26,13 @@ import java.util.zip.ZipInputStream;
 import javax.jcr.ImportUUIDBehavior;
 import javax.jcr.Session;
 
+import org.exoplatform.portal.config.UserACL;
 import org.exoplatform.services.log.ExoLogger;
 import org.exoplatform.services.log.Log;
+import org.exoplatform.social.core.space.SpaceUtils;
+import org.exoplatform.social.core.space.impl.DefaultSpaceApplicationHandler;
+import org.exoplatform.social.core.space.model.Space;
+import org.exoplatform.social.core.space.spi.SpaceService;
 import org.exoplatform.wiki.mow.api.Wiki;
 import org.exoplatform.wiki.mow.api.WikiType;
 import org.exoplatform.wiki.mow.core.api.MOWService;
@@ -81,7 +88,7 @@ public class WikiDataImportResource implements OperationHandler {
         int index = path.indexOf("/");
         String param0 = path.substring(0, index);
         String param1 = path.substring(index + 1);
-        WikiContainer<Wiki> wikiContainer = null;
+        createSpace(param1, operationContext);
         Wiki wiki = mowService.getModel().getWikiStore().getWikiContainer(wikiType).getWiki(param1, true);
         Session session = mowService.getSession().getJCRSession();
         String nodePath = wiki.getWikiHome().getJCRPageNode().getParent().getPath();
@@ -89,19 +96,31 @@ public class WikiDataImportResource implements OperationHandler {
           if (replaceExisting) {
             log.info("Overwrite existing script '" + path + "'.");
             wiki.getWikiHome().getJCRPageNode().remove();
+            
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            int size = 1024;
+            byte[] buffer = new byte[size];
+            int len;
+            while ((len = zin.read(buffer)) > -1 ) {
+                baos.write(buffer, 0, len);
+                size = 2 * size;
+                buffer = new byte[size];
+            }
+            baos.flush();
+            InputStream is = new ByteArrayInputStream(baos.toByteArray());
+            
+            session.importXML(nodePath, is, ImportUUIDBehavior.IMPORT_UUID_CREATE_NEW);
             session.save();
+            zin.closeEntry();
           } else {
             log.info("Ignore existing script'" + path + "'.");
             continue;
           }
         }
         
-        session.importXML(nodePath, zin, ImportUUIDBehavior.IMPORT_UUID_CREATE_NEW);
-        session.save();
-        break;
       }
       resultHandler.completed(NoResultModel.INSTANCE);
-
+      zin.close();
     } catch (Throwable exception) {
       throw new OperationException(OperationNames.IMPORT_RESOURCE, "Error while proceeding wiki import.", exception);
     }
@@ -109,6 +128,30 @@ public class WikiDataImportResource implements OperationHandler {
       throw new OperationException(OperationNames.IMPORT_RESOURCE, "No data stream available for wiki import.");
     }
 
+  }
+  
+  private void createSpace(String spaceName, OperationContext operationContext) {
+    if (spaceName.indexOf("/") > -1) {
+      spaceName = spaceName.substring(spaceName.lastIndexOf("/") + 1);
+    }
+    if (WikiType.GROUP.equals(wikiType)) {
+      SpaceService spaceService = operationContext.getRuntimeContext().getRuntimeComponent(SpaceService.class);
+      UserACL userAcl = operationContext.getRuntimeContext().getRuntimeComponent(UserACL.class);
+      Space space = spaceService.getSpaceByPrettyName(SpaceUtils.cleanString(spaceName));
+      if (space == null) {
+        log.info("Automatically create new space: '" + spaceName + "'.");
+        space = new Space();
+        space.setPrettyName(spaceName);
+        space.setDisplayName(spaceName);
+        space.setGroupId("/spaces/" + spaceName);
+        space.setRegistration(Space.OPEN);
+        space.setDescription(spaceName);
+        space.setType(DefaultSpaceApplicationHandler.NAME);
+        space.setVisibility(Space.PUBLIC);
+        space.setPriority(Space.INTERMEDIATE_PRIORITY);
+        spaceService.createSpace(space, userAcl.getSuperUser());
+      }
+    }
   }
 
 }
