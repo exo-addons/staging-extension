05382b82afc0847e17f210047ccbf7ae1c6bd393
gustavo <gustavo@4linux.com.br>
2014-11-05 18:26:16 -0200

    Create a tooltips.properties to keep the contextual help messages to show at page for the users.
    These codes is related with Issue "Add a contextual help on options #15"
    https://github.com/exo-addons/staging-extension/issues/15

--- portlet/src/main/java/org/exoplatform/management/portlet/assets/js/staging.js
+++ portlet/src/main/java/org/exoplatform/management/portlet/assets/js/staging.js
@@ -2,7 +2,7 @@
 // - it allows to correctly reinit the ng app after a (portal) page edition. Indeed, if we use the automatic init,
 //   when the (portal) page is edited and then saved, the module are not registered again, so they are not executed
 // - it is a good practice if we want to allow several ng app in the same html page (which could be the case with several portlets)
-require( ["SHARED/jquery", "stagingControllers", "stagingServices"], function ( $,  stagingControllers, stagingServices)
+require( ["SHARED/jquery", "stagingControllers", "stagingServices", "SHARED/bootstrap", "SHARED/bts_tooltip"], function ( $,  stagingControllers, stagingServices)
 {
   var stagingAppRoot = $('#staging');
   var stagingApp = angular.module('stagingApp', []);
