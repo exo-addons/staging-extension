f552ac9eb15606a6fe66911031e9bf33f934eb00
gustavo <gustavo@4linux.com.br>
2014-11-07 14:17:10 -0200

    Improvement at Staging Extension with a new feature that permit export only published contents. This improvement is related with Issue #13 "Allow to only select published contents for synchronization"

--- portlet/src/main/java/org/exoplatform/management/portlet/assets/js/controllers.js
+++ portlet/src/main/java/org/exoplatform/management/portlet/assets/js/controllers.js
@@ -14,7 +14,8 @@ define( "stagingControllers", [ "SHARED/jquery", "SHARED/juzu-ajax"], function (
             $scope.include_taxonomy = data.include_taxonomy;
             $scope.without_versionHistory = data.without_versionHistory;
             $scope.clean_publication = data.clean_publication;
-            
+            $scope.published_contents = data.published_contents;
+            $scope.only_published_contents = data.only_published_contents;
           }).error(function (data) {
           });
          
