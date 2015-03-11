0485e9b98706aedaa2b9b13daa3f2e0c15d65bca
boubaker <bkhanfir@exoplatform.com>
2015-03-11 10:17:36 +0100

    Secure password storage

--- component/service/src/main/java/org/exoplatform/management/service/impl/ChromatticServiceImpl.java
+++ component/service/src/main/java/org/exoplatform/management/service/impl/ChromatticServiceImpl.java
@@ -1,10 +1,20 @@
 package org.exoplatform.management.service.impl;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.security.KeyStore;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Properties;
 
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
 import javax.jcr.LoginException;
 import javax.jcr.NoSuchWorkspaceException;
 import javax.jcr.RepositoryException;
@@ -15,6 +25,7 @@ import org.chromattic.api.ChromatticBuilder;
 import org.chromattic.api.ChromatticSession;
 import org.chromattic.api.query.QueryResult;
 import org.exoplatform.commons.chromattic.ChromatticManager;
+import org.exoplatform.commons.utils.PropertyManager;
 import org.exoplatform.management.service.api.ChromatticService;
 import org.exoplatform.management.service.api.TargetServer;
 import org.exoplatform.management.service.api.model.TargetServerChromattic;
@@ -25,6 +36,10 @@ import org.exoplatform.services.jcr.core.ManageableRepository;
 import org.exoplatform.services.jcr.ext.common.SessionProvider;
 import org.exoplatform.services.log.ExoLogger;
 import org.exoplatform.services.log.Log;
+import org.exoplatform.web.security.codec.AbstractCodec;
+import org.exoplatform.web.security.codec.AbstractCodecBuilder;
+import org.exoplatform.web.security.security.TokenServiceInitializationException;
+import org.gatein.common.io.IOTools;
 import org.picocontainer.Startable;
 
 /**
@@ -54,6 +69,8 @@ public class ChromatticServiceImpl implements ChromatticService, Startable {
 
   Chromattic chromattic;
 
+  private AbstractCodec codec;
+
   public ChromatticServiceImpl(ChromatticManager chromatticManager, RepositoryService repositoryService) {
     // Nothing to do with chromatticManager, it's only to ensure that
     // ChromatticManager is started before this service
@@ -78,6 +95,12 @@ public class ChromatticServiceImpl implements ChromatticService, Startable {
     builder.setOptionValue(ChromatticBuilder.ROOT_NODE_PATH, STAGING_SERVERS_ROOT_PATH);
 
     chromattic = builder.build();
+
+    try {
+      initCodec();
+    } catch (Exception e) {
+      this.codec = null;
+    }
   }
 
   @Override
@@ -92,7 +115,8 @@ public class ChromatticServiceImpl implements ChromatticService, Startable {
       QueryResult<TargetServerChromattic> servers = session.createQueryBuilder(TargetServerChromattic.class).where("jcr:path like '" + STAGING_SERVERS_ROOT_PATH + "/%'").get().objects();
       while (servers.hasNext()) {
         TargetServerChromattic server = servers.next();
-        targetServers.add(new TargetServer(server.getId(), server.getName(), server.getHost(), server.getPort(), server.getUsername(), server.getPassword(), server.isSsl()));
+        String password = decodePassword(server.getPassword());
+        targetServers.add(new TargetServer(server.getId(), server.getName(), server.getHost(), server.getPort(), server.getUsername(), password, server.isSsl()));
       }
 
       session.save();
@@ -120,7 +144,8 @@ public class ChromatticServiceImpl implements ChromatticService, Startable {
       }
       server = servers.next();
       if (server != null) {
-        targetServer = new TargetServer(server.getId(), server.getName(), server.getHost(), server.getPort(), server.getUsername(), server.getPassword(), server.isSsl());
+        String password = decodePassword(server.getPassword());
+        targetServer = new TargetServer(server.getId(), server.getName(), server.getHost(), server.getPort(), server.getUsername(), password, server.isSsl());
       }
     } finally {
       if (session != null) {
@@ -151,7 +176,8 @@ public class ChromatticServiceImpl implements ChromatticService, Startable {
       server.setHost(targetServer.getHost());
       server.setPort(targetServer.getPort());
       server.setUsername(targetServer.getUsername());
-      server.setPassword(targetServer.getPassword());
+      String password = encodePassword(targetServer.getPassword());
+      server.setPassword(password);
       server.setSsl(targetServer.isSsl());
 
       session.save();
@@ -216,4 +242,99 @@ public class ChromatticServiceImpl implements ChromatticService, Startable {
 
   @Override
   public void stop() {}
+
+  private void initCodec() throws Exception {
+    String builderType = PropertyManager.getProperty("gatein.codec.builderclass");
+    Map<String, String> config = new HashMap<String, String>();
+
+    if (builderType != null) {
+      // If there is config for codec in configuration.properties, we read the
+      // config parameters from config file
+      // referenced in configuration.properties
+      String configFile = PropertyManager.getProperty("gatein.codec.config");
+      InputStream in = null;
+      try {
+        File f = new File(configFile);
+        in = new FileInputStream(f);
+        Properties properties = new Properties();
+        properties.load(in);
+        for (Map.Entry<?, ?> entry : properties.entrySet()) {
+          config.put((String) entry.getKey(), (String) entry.getValue());
+        }
+        config.put("gatein.codec.config.basedir", f.getParentFile().getAbsolutePath());
+      } catch (IOException e) {
+        throw new TokenServiceInitializationException("Failed to read the config parameters from file '" + configFile + "'.", e);
+      } finally {
+        IOTools.safeClose(in);
+      }
+    } else {
+      // If there is no config for codec in configuration.properties, we
+      // generate key if it does not exist and setup the
+      // default config
+      builderType = "org.exoplatform.web.security.codec.JCASymmetricCodecBuilder";
+      String gtnConfDir = PropertyManager.getProperty("gatein.conf.dir");
+      if (gtnConfDir == null || gtnConfDir.length() == 0) {
+        throw new TokenServiceInitializationException("'gatein.conf.dir' property must be set.");
+      }
+      File f = new File(gtnConfDir + "/codec/codeckey.txt");
+      if (!f.exists()) {
+        File codecDir = f.getParentFile();
+        if (!codecDir.exists()) {
+          codecDir.mkdir();
+        }
+        OutputStream out = null;
+        try {
+          KeyGenerator keyGen = KeyGenerator.getInstance("AES");
+          keyGen.init(128);
+          SecretKey key = keyGen.generateKey();
+          KeyStore store = KeyStore.getInstance("JCEKS");
+          store.load(null, "gtnStorePass".toCharArray());
+          store.setEntry("gtnKey", new KeyStore.SecretKeyEntry(key), new KeyStore.PasswordProtection("gtnKeyPass".toCharArray()));
+          out = new FileOutputStream(f);
+          store.store(out, "gtnStorePass".toCharArray());
+        } catch (Exception e) {
+          throw new TokenServiceInitializationException(e);
+        } finally {
+          IOTools.safeClose(out);
+        }
+      }
+      config.put("gatein.codec.jca.symmetric.keyalg", "AES");
+      config.put("gatein.codec.jca.symmetric.keystore", "codeckey.txt");
+      config.put("gatein.codec.jca.symmetric.storetype", "JCEKS");
+      config.put("gatein.codec.jca.symmetric.alias", "gtnKey");
+      config.put("gatein.codec.jca.symmetric.keypass", "gtnKeyPass");
+      config.put("gatein.codec.jca.symmetric.storepass", "gtnStorePass");
+      config.put("gatein.codec.config.basedir", f.getParentFile().getAbsolutePath());
+    }
+
+    try {
+      this.codec = Class.forName(builderType).asSubclass(AbstractCodecBuilder.class).newInstance().build(config);
+      LOG.info("Initialized CookieTokenService.codec using builder " + builderType);
+    } catch (Exception e) {
+      throw new TokenServiceInitializationException("Could not initialize CookieTokenService.codec.", e);
+    }
+  }
+
+  private String decodePassword(String password) {
+    if (codec != null) {
+      try {
+        password = codec.decode(password);
+      } catch (Exception e) {
+        LOG.warn("Error while decoding password, it will be used in plain text", e);
+      }
+    }
+    return password;
+  }
+
+  private String encodePassword(String password) {
+    if (codec != null) {
+      try {
+        password = codec.encode(password);
+      } catch (Exception e) {
+        LOG.warn("Error while encoding password, it will be used in plain text", e);
+      }
+    }
+    return password;
+  }
+
 }
