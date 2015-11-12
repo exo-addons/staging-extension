a558e7bfcc1fa5156853196e51b06750e2b86deb
boubaker <bkhanfir@exoplatform.com>
2015-11-12 10:00:13 +0100

    Test space existence before getting identity

--- component/common/src/main/java/org/exoplatform/management/common/AbstractOperationHandler.java
+++ component/common/src/main/java/org/exoplatform/management/common/AbstractOperationHandler.java
@@ -108,12 +108,17 @@ public abstract class AbstractOperationHandler implements OperationHandler {
 
   protected final Identity getIdentity(String id) {
     try {
-      Identity userIdentity = identityStorage.findIdentity(OrganizationIdentityProvider.NAME, id);
+      Identity identity = identityStorage.findIdentity(OrganizationIdentityProvider.NAME, id);
 
-      if (userIdentity != null) {
-        return userIdentity;
+      if (identity != null) {
+        return identity;
       } else {
-        Identity spaceIdentity = null;
+        Identity spaceIdentity = identityStorage.findIdentity(SpaceIdentityProvider.NAME, id);
+
+        if (spaceIdentity != null) {
+          return spaceIdentity;
+        }
+
         // Try to see if space was renamed
         Space space = spaceService.getSpaceByGroupId(id);
         if (space == null) {
@@ -124,6 +129,8 @@ public abstract class AbstractOperationHandler implements OperationHandler {
         }
         if (space != null) {
           spaceIdentity = identityStorage.findIdentity(SpaceIdentityProvider.NAME, space.getPrettyName());
+        } else {
+          log.warn("Space '" + id + "'was not found");
         }
         return spaceIdentity;
       }
